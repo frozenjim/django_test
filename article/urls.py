98dc9ef72f687954da955ade9ca04dc25bbeb435
frozenjim <james@whitehousenorth.com>
2015-09-02 10:30:21 -0400

    Tutorial 8: cookies and sessions (part1 - cookies)
    Setting language in cookie is working fine.
    
    Also, cleaned up the STATICFILES_DIRS section a bit more.

--- article/urls.py
+++ article/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.hello, name='hello'),
     url(r'^all/$', views.articles, name='articles'),
-    url(r'get/(?P<article_id>[0-9+])', views.article, name='get_article')
+    url(r'get/(?P<article_id>[0-9+])', views.article, name='get_article'),
+    url(r'^language/(?P<language>[a-z\-]+)/$', 'article.views.language', name='set_language'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.template.loader import get_template
 from django.views.generic import TemplateView
 
@@ -7,16 +7,39 @@ from article.models import Article
 
 
 def articles(request):
-    return render_to_response(
+    """
+    I updated render_to_request to "render" because
+    render_to_request will be deprecated in v2.0
+    according to the devs.
+    """
+
+    language = 'en-us'
+    session_language = 'en-us'
+
+    if 'lang' in request.COOKIES:
+        language = request.COOKIES['lang']
+
+    return render(
+        request,
         'articles.html',
-        {'articles': Article.objects.all()})
+        {'articles': Article.objects.all(),
+            'language': language}
+    )
 
 
 def article(request, article_id=1):
-    return render_to_response(
+    return render(
+        request,
         'article.html',
         {'article': Article.objects.get(id=article_id)}
     )
+
+
+def language(request, language='en-us'):
+    response = HttpResponse("setting language to %s" % language)
+
+    response.set_cookie('lang', language)
+    return response
 
 
 def hello(request):
