a6e4ebfe95351a21c6968c79f58f8747c76b8bea
frozenjim <james@whitehousenorth.com>
2015-09-02 13:25:08 -0400

    Tutorial 8: cookies and sessions (Complete)
    Session and Cookie variables are set and retrieved.

--- django_test/urls.py
+++ django_test/urls.py
@@ -20,14 +20,20 @@ from article.views import HelloTemplate
 
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+
+    # User auth urls
+    url(r'^accounts/login/$', 'django_test.views.login'),
+    url(r'^accounts/auth/$', 'django_test.views.auth_view'),
+    url(r'^accounts/logout/$', 'django_test.views.logout'),
+    url(r'^accounts/loggedin/$', 'django_test.views.loggedin'),
+    url(r'^accounts/invalid/$', 'django_test.views.invalid_login'),
+
+    # From previous tutorials
     url(r'^$', 'django_test.views.home', name='home'),
     url(r'^hello/$', 'article.views.hello', name='fake_hello'),
     url(r'^hello_template/$', 'article.views.hello_template', name='hello_template'),
     url(r'^hello_template_simple/$', 'article.views.hello_template_simple', name='hello_template_simple' ),
     url(r'^hello_class_view/$', HelloTemplate.as_view(), name='hello_class_view'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^articles/', include('article.urls', namespace='articles')),
-    # url(r'', include('play.urls', namespace='default')),
-    # url(r'^play/', include('play.urls', namespace='play')),
-    # url(r'^userprofile/', include('userprofile.urls', namespace='userprofile')),
 ]
--- django_test/views.py
+++ django_test/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
+
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'login.html', c,)
+
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/accounts/loggedin')
+    else:
+        return HttpResponseRedirect('/accounts/invalid')
+
+
+def loggedin(request):
+    return render(request, 'loggedin.html', {'full_name': request.user.username})
+
+
+def invalid_login(request):
+    return render(request, 'invalid_login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'logout.html')
 
 
 def home(request):
     return render(request, 'home.html')
-
